a0f863bc5e1dd608c5e35e9e78472e71cccae035
lmcallme <l.m.zhongguo@gmail.com>
2017-07-23 01:01:41 +0800

    add vimrc and vim-header install script

---
+++ vim/vim-header.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+# File: vim-header.bash
+# Author: lmcallme <l.m.zhongguo@gmial.com>
+# Date: 23.07.2017
+# Last Modified Date: 23.07.2017
+# Last Modified By: lmcallme <l.m.zhongguo@gmial.com>
+
+INSTALL_PATH=${PPATH}/vim-header
+GIT_URL=https://github.com/alpertuna/vim-header
+
+if [ ! -d INSTALL_PATH]; then
+    # Install vim-header
+    git clone --depth=1 ${GIT_URL} ${INSTALL_PATH}
+    
+    # Config vim-header
+    echo "let g:header_field_author = '${NAME}'" >> ${MY_CONFIG}
+    echo "let g:header_field_author_email = '${EMAIL}'" >> ${MY_CONFIG}
+    # echo "map <F4> :AddHeader" >> ${MY_CONFIG}
+
+    echo "installed vim-header"
+fi
---
+++ vim/vimrc.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+# File: vimrc.bash
+# Author: lmcallme <l.m.zhongguo@gmial.com>
+# Date: 23.07.2017
+# Last Modified Date: 23.07.2017
+# Last Modified By: lmcallme <l.m.zhongguo@gmial.com>
+
+cd $(dirname "$0") || exit 1
+
+export NAME=lmcallme
+export EMAIL=l.m.zhongguo@gmial.com
+
+if [[ ! -d ~/.vim_runtime ]]; then
+    
+    # Install amix-vimrc
+
+    git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+    sh ~/.vim_runtime/install_awesome_vimrc.sh
+    #sh ~/vim_runtime/install_basic_vimrc.sh
+fi
+
+# amix-vim's pathogen path
+export PPATH=~/.vim_runtime/sources_non_forked
+
+# amix-vim's append vimrc_file
+export MY_CONFIG=~/.vim_runtime/my_configs.vim
+
+source vim-header.bash
