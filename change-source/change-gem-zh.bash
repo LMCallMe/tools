111426a16b044305d518fcbe7a15f4e66d8a16d6
lmcallme <l.m.zhongguo@gmail.com>
2017-07-23 01:03:07 +0800

    add a script to change gem source to ruby-china source by bundle

---
+++ change-source/change-gem-zh.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+# File: change-gem-zh.bash
+# Author: lmcallme <l.m.zhongguo@gmial.com>
+# Date: 23.07.2017
+# Last Modified Date: 23.07.2017
+# Last Modified By: lmcallme <l.m.zhongguo@gmial.com>
+
+bundle config mirror.https://rubygems.org https://gems.ruby-china.org
